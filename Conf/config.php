751318df65ecac099945d3131d1ebb4086a02d5d
ssor <ssor@qq.com>
2013-06-02 23:49:21 +0800

    websocket分为服务端和客户端，对应网页的改动

--- Conf/config.php
+++ Conf/config.php
@@ -5,8 +5,8 @@ return array(
 	'DB_DSN' => 'sqlite:'.dirname(__FILE__).'/test.db3', //相对于config目录的路径
 	'DB_NAME'=>'test.db3',
 	// 'WS_LOCATION' => 'ws://192.168.1.102:4649'
-	// 'WS_LOCATION' => 'ws://192.168.1.100:4649'
-	'WS_LOCATION' => 'ws://192.168.248.97:4649'
+	'WS_LOCATION' => 'ws://192.168.1.100:4649'
+	// 'WS_LOCATION' => 'ws://192.168.248.97:4649'
 
 );
 ?>
