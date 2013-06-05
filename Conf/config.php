035c38c960cbbae6c0f240491bad7190162c5470
ssor <ssor@qq.com>
2013-06-05 09:18:26 +0800

    支持gps和UHF

--- Conf/config.php
+++ Conf/config.php
@@ -4,8 +4,8 @@ return array(
 	'DB_TYPE'=>'pdo',
 	'DB_DSN' => 'sqlite:'.dirname(__FILE__).'/test.db3', //相对于config目录的路径
 	'DB_NAME'=>'test.db3',
-	// 'WS_LOCATION' => 'ws://192.168.1.102:4649'
-	'WS_LOCATION' => 'ws://192.168.1.100:4649'
+	'WS_LOCATION' => 'ws://111.67.197.251:4649'
+	// 'WS_LOCATION' => 'ws://192.168.1.100:4649'
 	// 'WS_LOCATION' => 'ws://192.168.248.97:4649'
 
 );
